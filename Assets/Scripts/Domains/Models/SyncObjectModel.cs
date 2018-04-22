185b029809f775ab23a22a33960cfe731818a6ee
udon <naoki_udon@outlook.jp>
2018-04-23 01:23:57 +0900

    create clean architecture base

--- Assets/Scripts/Domains/Models/SyncObjectModel.cs
+++ Assets/Scripts/Domains/Models/SyncObjectModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PositionModel : MonoBehaviour {
+public class SyncObjectModel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
