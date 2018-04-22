056876e12666abfef370e2f79cb7ef8c270bffce
udon <naoki_udon@outlook.jp>
2018-04-23 00:46:05 +0900

    create domain data

---
+++ Assets/Scripts/Domains/SystemStructs.cs
@@ -0,0 +1,40 @@
+﻿namespace AGS.Domains{
+
+    /// <summary>
+    /// ルーム情報、通信単位の情報や同期する対戦でのシステム情報
+    /// </summary>
+    public struct Room{
+        public int RoomId;
+
+        public UnityEngine.Vector3 RoomRoot;
+
+        public float TotalGameTime;
+
+        public float CurrentGameTime;
+    }
+
+    /// <summary>
+    /// ユーザー情報、ゲーム内パラメータやシステム上のユーザーパラメータ
+    /// </summary>
+    public struct User{
+        // 属するルーム
+        public Room Room;
+        // ユーザーの識別ID
+        public int UserId;
+        // ユーザーの表示名
+        public string UserName;
+        // プレイヤー情報
+        public Player PlayerParam;
+       
+    }
+
+    /// <summary>
+    /// プレイヤー情報、ゲーム中のプレイヤーに関するデータ
+    /// </summary>
+    public struct Player{
+        public float Hp;
+
+        public UnityEngine.Vector3 Position;
+        
+    }
+}
