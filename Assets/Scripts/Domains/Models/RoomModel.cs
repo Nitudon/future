631fd1b91f91a31be3e8e14cd1079919144c1f18
udon <naoki_udon@outlook.jp>
2018-04-30 18:32:02 +0900

    create UI Root

--- Assets/Scripts/Domains/Models/RoomModel.cs
+++ Assets/Scripts/Domains/Models/RoomModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using AGS.Domains;
 using UnityEngine;
 using UniRx;
---
+++ Assets/Scripts/Presentation/Presenters/GameRulePresenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using AGS.Domains;
+using UnityEngine;
+using Zenject;
+
+public class GameRulePresenter : MonoBehaviour {
+
+    [Inject]
+    private RoomModel _roomModel;
+
+    public async Task SetupGame(RoomData roomdata)
+    {
+        _roomModel.Initialize(roomdata);
+    }
+}
