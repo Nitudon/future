50518c0e483c5d8bf3ee790b6b0a2501294015b9
udon <naoki_udon@outlook.jp>
2018-04-30 19:56:05 +0900

    update submodule

--- Assets/Scripts/Presentation/Presenters/GameScenePresenter.cs
+++ Assets/Scripts/Presentation/Presenters/GameScenePresenter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UdonLib.Commons;
 
@@ -9,4 +10,18 @@ public class GameScenePresenter : SceneManager {
     private static readonly string ROOM_SCENE_NAME = "RoomMatching";
     private static readonly string GAME_SCENE_NAME = "MultiPlayersGame";
 
+    public async Task LoadTitleAsync()
+    {
+        await LoadSceneObservable(TITLE_SCENE_NAME);
+    }
+
+    public async Task LoadRoomAsync()
+    {
+
+    }
+
+    public async Task LoadGameAsync()
+    {
+
+    }
 }
