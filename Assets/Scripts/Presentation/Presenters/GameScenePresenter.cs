ff89d17bf6dce95b6f003b67fe16dea49710afb4
udon <naoki_udon@outlook.jp>
2018-04-30 19:14:27 +0900

    create Scene load System

---
+++ Assets/Scripts/Presentation/Presenters/GameScenePresenter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+
+public class GameScenePresenter : SceneManager {
+
+    private static readonly string TITLE_SCENE_NAME = "Title";
+    private static readonly string ROOM_SCENE_NAME = "RoomMatching";
+    private static readonly string GAME_SCENE_NAME = "MultiPlayersGame";
+
+}
