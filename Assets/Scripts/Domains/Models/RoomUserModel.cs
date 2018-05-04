df9400d9aa4d925815a5381ce137f5f440deabc5
udon <naoki_udon@outlook.jp>
2018-05-04 23:47:26 +0900

    join and left system base

--- Assets/Scripts/Domains/Models/RoomUserModel.cs
+++ Assets/Scripts/Domains/Models/RoomUserModel.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class RoomUserModel : MonoBehaviour {
+
+    private string _roomId;
+    public string RoomId => _roomId;
+
+    private RoomAccessController _roomAccessController;
+    public RoomAccessController roomAccessController => _roomAccessController;
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/Domains/Usecases/RoomAccessController.cs
+++ Assets/Scripts/Domains/Usecases/RoomAccessController.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using AGS.Domains;
 using UnityEngine;
+using UniRx;
 
-public class RoomAccessController : MonoBehaviour {
+public class RoomAccessController
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private ReactiveCollection<string> _roomPlayers;
+    public IReadOnlyReactiveCollection<string> RoomPlayers => _roomPlayers;
+
+    public void CreateRoom()
+    {
+        _roomPlayers = new ReactiveCollection<string>();
+    }
+
+    public int JoinRoom(string name)
+    {
+        if (_roomPlayers == null)
+        {
+            return -1;
+        }
+
+        _roomPlayers.Add(name);
+        return _roomPlayers.Count;
+    }
+
+    public void LeftRoom(int id)
+    {
+        if (_roomPlayers == null || _roomPlayers.Count < id)
+        {
+            return;
+        }
+
+
+    }
 }
