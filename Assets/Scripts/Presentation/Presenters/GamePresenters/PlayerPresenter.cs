ee949094d421c6a63b92d4afdd0b22c0bafd5b21
udon <naoki_udon@outlook.jp>
2018-05-06 19:24:30 +0900

    edit PlayerUI View

--- Assets/Scripts/Presentation/Presenters/GamePresenters/PlayerPresenter.cs
+++ Assets/Scripts/Presentation/Presenters/GamePresenters/PlayerPresenter.cs
@@ -9,6 +9,9 @@ public class PlayerPresenter : MonoBehaviour, System.IDisposable {
     [SerializeField]
     private PlayerModel _gamePlayerModel;
 
+    [SerializeField]
+    private GamePlayerUI _gamePlayerUI;
+
     private CompositeDisposable _disposable = new CompositeDisposable();
 
     public void Initialize()
@@ -17,10 +20,24 @@ public class PlayerPresenter : MonoBehaviour, System.IDisposable {
             .Subscribe(_gamePlayerView.SyncPosition)
             .AddTo(_disposable);
 
+        if (_gamePlayerModel.IsMine)
+        {
+            _gamePlayerModel.PlayerHp
+                .Subscribe(_gamePlayerUI.UpdatePlayerUI)
+                .AddTo(_disposable);
+        }
+    
         _gamePlayerModel.OnActivated
             .Where(active => active)
             .First()
-            .Subscribe(_ => _gamePlayerView.ActivatePlayerView())
+            .Subscribe(_ =>
+                {
+                    _gamePlayerView.ActivatePlayerView();
+                    if(_gamePlayerModel.IsMine)
+                    {
+                        _gamePlayerUI.Activate();
+                    }
+                })
             .AddTo(_disposable);
     }
 
--- Assets/Scripts/Presentation/Views/InGame/UI/GamePlayerUI.cs
+++ Assets/Scripts/Presentation/Views/InGame/UI/GamePlayerUI.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePlayerUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private Text _hpUI;
+
+    [SerializeField]
+    private Image _popupUI;
+
+	public void Activate()
+    {
+        _hpUI.enabled = true;
+        _popupUI.enabled = true;
+    }
+
+    public void UpdatePlayerUI(float hp)
+    {
+        _hpUI.text = hp.ToString();
+    }
 }
