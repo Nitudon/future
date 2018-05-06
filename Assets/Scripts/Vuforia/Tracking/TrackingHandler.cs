9e1bf0812f83aeefef9c9af38f6e3056e9f8c412
udon <naoki_udon@outlook.jp>
2018-05-06 16:37:46 +0900

    create tracking handle

---
+++ Assets/Scripts/Vuforia/Tracking/TrackingHandler.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Vuforia;
+
+public class TrackingHandler : MonoBehaviour, ITrackableEventHandler{
+
+    #region MEMBER_VARIABLES
+
+    public BoolReactiveProperty OnTrackingFoundStatusChanged = new BoolReactiveProperty(false);
+
+    private TrackableBehaviour _trackingBehaviour;
+
+    #endregion MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    private void Start()
+    {
+        _trackingBehaviour = GetComponent<ImageTargetBehaviour>();
+        if (_trackingBehaviour)
+        {
+            _trackingBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_trackingBehaviour)
+        {
+            _trackingBehaviour.UnregisterTrackableEventHandler(this);
+        }
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + _trackingBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + _trackingBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    private void OnTrackingFound()
+    {
+        OnTrackingFoundStatusChanged.Value = true;
+    }
+
+
+    private void OnTrackingLost()
+    {
+        OnTrackingFoundStatusChanged.Value = false;
+    }
+
+    #endregion // PROTECTED_METHODS
+}
