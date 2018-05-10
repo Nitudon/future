c257d8ab77534b54485115e48dcbe23913a821eb
udon <naoki_udon@outlook.jp>
2018-05-10 23:33:10 +0900

    fix sync error

--- Assets/Scripts/Presentation/Views/InGame/SyncObjectsView.cs
+++ Assets/Scripts/Presentation/Views/InGame/SyncObjectsView.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using AGS.Domains;
 using UnityEngine;
 using UniRx;
 
 public class SyncObjectsView : MonoBehaviour {
 
-    public void SyncPosition(SyncObjectModel sync){
+    public void SyncPosition(SyncObjectModel<SyncObjectData> sync){
         
     }
 
