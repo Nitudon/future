798d5dec60e18b7645af008289e896eae9eeec7d
udon <naoki_udon@outlook.jp>
2018-05-12 02:16:49 +0900

    fix http request

--- Assets/Scripts/Server/WWW/RoomWebRequest.cs
+++ Assets/Scripts/Server/WWW/RoomWebRequest.cs
@@ -13,17 +13,17 @@ namespace AGS.WebRequest
         
         public static async Task<RoomData> FetchRoomDataAsync(string id)
         {
-            return await HttpRequestAsync.GetRequestAsync<RoomData>($"https://{URL_DOMAIN}/?room_id={id}");
+            return await HttpRequestAsync.GetRequestAsync<RoomData>($"http://{URL_DOMAIN}/?room_id={id}");
         }
 
         public static async Task<RoomData> TestRoomDataAsync()
         {
-            return await HttpRequestAsync.GetRequestAsync<RoomData>($"https://{URL_DOMAIN}/test");
+            return await HttpRequestAsync.GetRequestAsync<RoomData>($"http://{URL_DOMAIN}/test");
         }
 
         public static async Task<RoomData> JoinRoomAsync()
         {
-            return await HttpRequestAsync.GetRequestAsync<RoomData>($"https://{URL_DOMAIN}/room/join");
+            return await HttpRequestAsync.GetRequestAsync<RoomData>($"http://{URL_DOMAIN}/room/join");
         }
 
     }
